805bbb0a3bd6a1c1e99e63f2840bde13a6cae107
Billy <iceqlips@gmail.com>
2024-12-18 19:47:47 +0500

    module 3 lesson 3

---
+++ module_3_3.py
@@ -0,0 +1,16 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+
+def string_info(string):
+    tuple(string.upper())
+    tuple(string.lower())
+
+def is_contains(string, list_to_search):
+    for item in list_to_search:
+        item.lower()
+    return string.lower()
+
+print(calls)
